3ec347e975c9cd3c3db8bf91fa4f30ee53920bea
vangona <rlarhksrud12@naver.com>
2021-12-14 01:17:01 +0900

    2021.12.14 add bookmark to community

--- src/routes/Community.js
+++ src/routes/Community.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import Answer from "../components/Answer";
 import Friends from "../components/Friends";
+import Loading from "../components/Loading";
 import Notes from "../components/Notes";
+import { dbService } from "../fBase";
 
 const Container = styled.div`
     display: flex;
@@ -15,19 +18,49 @@ const Container = styled.div`
 `;
 
 const Community = ({userObj, refreshFriends, noteData}) => {
+    const [isLoading, setIsLoading] = useState(true);
     const [friendLoading, setFriendLoading] = useState(false);
+    const [someoneAnswers, setSomeoneAnswers] = useState('');
 
     const getFriendLoading = (loading) => {
         setFriendLoading(loading);
     }
 
-    useEffect(() => {
+    const getSomeoneAnswers = async () => {
+        await dbService.collection("answers").where("answerId", "in", userObj.bookmarks).get()
+        .then(snapshot => {
+            const someoneAnswerArray = snapshot.docs.map(doc => ({
+                id: doc.answerId,
+                ...doc.data()    
+            }))
+            someoneAnswerArray.sort((a, b) => {
+                if(a.createdAt > b.createdAt) return -1;
+                if(a.createdAt === b.createdAt) return 0;
+                if(a.createdAt < b.createdAt) return 1;
+              });
+            setSomeoneAnswers(someoneAnswerArray);
+            setIsLoading(!isLoading);
+        })
+    }
+
+    useEffect(()=>{
+        getSomeoneAnswers();
     }, [])
 
     return (
         <Container>
-            <Friends userObj={userObj} loading={friendLoading} refreshFriends={refreshFriends} getFriendLoading={getFriendLoading} />
-            <Notes userObj={userObj} noteData={noteData} />
+            {isLoading
+            ? <Loading />
+            : 
+                <>
+                    <Friends userObj={userObj} loading={friendLoading} refreshFriends={refreshFriends} getFriendLoading={getFriendLoading} />
+                    <Notes userObj={userObj} noteData={noteData} />
+                    {someoneAnswers.map(answer => 
+                        <Answer key={answer.answerId} answer={answer} userObj={userObj}/>
+                    )
+                    }
+                </>
+            }
         </Container>
     )
 }
